b40ceafef494ef3b7d4fb2a2f97627f3c3fe67c3
meganlr <megan_power504@hotmail.com>
2018-11-22 16:54:31 +0000

    Change title to Yo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello! <code>src/App.js</code> and save to reload.
+            Yo <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
