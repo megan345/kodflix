aa2f16fe0bc1e705a611312bec500848d8118a74
meganlr <megan_power504@hotmail.com>
2019-01-08 15:08:10 +0000

    Add the TV show title overlay on hover-over #5

--- src/App.js
+++ src/App.js
@@ -15,24 +15,42 @@ class App extends Component {
         <div class="container">
           <div class="form-row">
             <div class="child">
-              <img src={ted} className="App-logo" alt="ted-movie" />
+              <img src={ted} className="image" alt="ted-movie" />
+              <div className="overlay">
+                <h1>TED The Movie</h1>
+              </div>
             </div>
             <div class="child">
-              <img src={goodwillhunting} className="App-logo" alt="goodwillhunting-movie" />
+              <img src={goodwillhunting} className="image" alt="goodwillhunting-movie" />
+              <div className="overlay">
+                <h1>Good Will Hunting</h1>
+              </div>
             </div>
             <div class="child">
-              <img src={matrix} className="App-logo" alt="matrix-movie" />
+              <img src={matrix} className="image" alt="matrix-movie" />
+              <div className="overlay">
+                <h1>The Matrix</h1>
+              </div>
             </div>
           </div>
           <div class="form-row">
             <div class="child">
-              <img src={hitch} className="App-logo" alt="hitch-movie" />
+              <img src={hitch} className="image" alt="hitch-movie" />
+              <div className="overlay">
+                <h1>Hitch</h1>
+              </div>
             </div>
             <div class="child">
-              <img src={nottinghill} className="App-logo" alt="nottinghill-movie" />
+              <img src={nottinghill} className="image" alt="nottinghill-movie" />
+              <div className="overlay">
+                <h1>Notting Hill</h1>
+              </div>
             </div>
             <div class="child">
-              <img src={casinoroyale} className="App-logo" alt="casinoroyale-movie" />
+              <img src={casinoroyale} className="image" alt="casinoroyale-movie" />
+              <div className="overlay">
+                <h1>Casino Royale</h1>
+              </div>
             </div>
           </div>
         </div>
