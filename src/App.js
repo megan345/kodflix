85bff4eae245af7efb3d31bc59dc7136a4084fd1
meganlr <megan_power504@hotmail.com>
2019-01-08 12:14:57 +0000

    Replaec TV show titles with covers #2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import moviecover from './moviecover.jpg';
+import casinoroyale from './Images/casinoroyale.jpg';
+import ted from './Images/ted.jpg';
+import goodwillhunting from './Images/goodwillhunting.jpg';
+import matrix from './Images/matrix.jpg';
+import hitch from './Images/hitch.jpg';
+import nottinghill from './Images/nottinghill.jpg';
 
 class App extends Component {
   render() {
@@ -23,19 +28,34 @@ class App extends Component {
       //   </header>
       // </div>
       <div className="App">
-          <div class="container">
-            <div class="form-row">
-              <div class="child">Black Mirror</div>
-              <div class="child">Breaking Bad</div>
-              <div class="child">Death Note</div>
-            </div>
-            <div class="form-row">
-              <div class="child">Game of Thrones</div>
-              <div class="child">The Walking Dead</div>
-              <div class="child">The Wire</div>
-            </div>
-         </div>
+        <div class="container">
+          <div class="form-row">
 
+            <div class="child">
+              <img src={ted} className="App-logo" alt="ted-movie" />
+            </div>
+            <div class="child">
+              <img src={goodwillhunting} className="App-logo" alt="goodwillhunting-movie" />
+            </div>
+            <div class="child">
+              <img src={matrix} className="App-logo" alt="matrix-movie" />
+            </div>
+
+          </div>
+
+          <div class="form-row">
+            <div class="child">
+              <img src={hitch} className="App-logo" alt="hitch-movie" />
+            </div>
+            <div class="child">
+              <img src={nottinghill} className="App-logo" alt="nottinghill-movie" />
+            </div>
+            <div class="child">
+              <img src={casinoroyale} className="App-logo" alt="casinoroyale-movie" />
+            </div>
+
+          </div>
+        </div>
       </div>
     );
   }
