f0bba7161a42ea8d6f6b369ae8308be09d1ee15a
meganlr <megan_power504@hotmail.com>
2019-02-05 12:17:01 +0000

    Enable the backend server in prod #23

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const getMovies = require('./movies.js');
+const path = require('path');
 
 // New command to start the server is npm run start-backend
 
@@ -10,6 +11,10 @@ app.get('/rest/movie-details', (req, res) =>
 
 app.get('/', (req, res) => 
     res.send("Hello, World"))
+
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('*', function (req, res) { 
+    res.sendFile(path.join(__dirname, '../../build', 'index.html')); });
 
 app.listen(port, () => 
     console.log(`Example app listening on port ${port}!`))
