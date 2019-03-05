51fe97f1195d76aa08d3576ede5c9e784d802cc7
meganlr <megan_power504@hotmail.com>
2019-03-05 22:03:53 +0000

    #Task24 Added common/images

--- src/frontend/Details/Details.js
+++ src/frontend/Details/Details.js
@@ -30,20 +30,23 @@ export default class Details extends Component {
     });
   }
   render() {
-    // let movie = this.state.movie;
+    let movie = this.state.movie;
     if (this.state.movie === undefined) {
       return <Redirect to="/not-found" />;
     } else if (this.state.movie) {
       return (
         <div className="Details">
           <h1>{this.state.movie.name}</h1>
-          <Link to="/">Link to home page</Link>
           <div className="content">
-            <div className="details-child">{this.state.movie.details}</div>
-            <div className="details-child">
-              <img src={this.state.movie.logo} alt="Movie"></img>
+            <div className="child">{this.state.movie.details}</div>
+            <div className="child">
+              <img
+                src={require(`../common/images/${movie.id}.jpg`)}
+                alt={`${movie.id}`}
+              />
             </div>
           </div>
+          <Link to="/">Link to home page</Link>
         </div>
       );
     }
--- src/frontend/Movie.js
+++ src/frontend/Movie.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Movie(props){
     return (
-      <Link to={`${props.id}`} class="child">
+      <Link to={`${props.id}`} className="child">
         <img src={props.logo} className="image" alt={props.name} id={props.id}/>
         <div className="overlay">
           <h1>{props.name}</h1>
