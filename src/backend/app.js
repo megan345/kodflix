9ac5fa89921c0b3578ef69731c35dc571aeab130
meganlr <megan_power504@hotmail.com>
2019-03-07 10:37:41 +0000

    Removed '/' path

--- src/backend/app.js
+++ src/backend/app.js
@@ -9,9 +9,6 @@ const path = require('path');
 app.get('/rest/shows', (req, res) => 
     res.send(getMovies()));
 
-app.get('/', (req, res) => 
-    res.send("Hello, World"));
-
 app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function (req, res) { 
     res.sendFile(path.join(__dirname, '../../build', 'index.html')); });
