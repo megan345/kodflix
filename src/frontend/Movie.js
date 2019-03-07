f44e747b72bfdb1a2749e2d040deabf986a41d09
meganlr <megan_power504@hotmail.com>
2019-03-07 11:18:27 +0000

    Changed movie.js

--- src/frontend/Movie.js
+++ src/frontend/Movie.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 export default function Movie(props){
     return (
-      <Link to={`${props.id}`} className="child">
-        <img src={props.logo} className="image" alt={props.name} id={props.id}/>
-        <div className="overlay">
+      <Link to={`${props.id}`} className="item">
+      <img src={require(`./common/images/${props.id}.jpg`)} alt={`${props.id}`} />        
+      <div className="overlay">
           <h1>{props.name}</h1>
         </div>
       </Link>
