3fe0e48976e918c132432600d4cdda19e1da518b
meganlr <megan_power504@hotmail.com>
2019-03-08 13:17:05 +0000

    Changes to details

--- src/frontend/Details/Details.js
+++ src/frontend/Details/Details.js
@@ -15,7 +15,9 @@ export default class Details extends Component {
 
   componentDidMount() {  
     fetch('/rest/shows')
-        .then(response => response.json())
+        .then(response => {
+          return response.json();
+        })
         .then(gallery => {
           let galleryImageId = this.props.match.params.movieId;
           let movie = gallery.find(show => show.id === galleryImageId);
@@ -24,20 +26,21 @@ export default class Details extends Component {
     }
 
   render() {
-    // let movie = this.state;
+    let movie = this.state.movie;
     if (this.state.movie === undefined) {
       return <Redirect to="/not-found" />;
-    } else if (this.state.movie) {
+    } else if (movie.id) {
+      console.log(this.state.movie);
       return (
         <div className="details">
-          <h1>{this.state.movie.name}</h1>
+          <h1>{movie.name}</h1>
           <div className="content">
-            <div className="child">{this.state.movie.synopsis}</div>
+            <div className="child">{movie.synopsis}</div>
             <div className="child">
-              <img
-                src={require(`../common/images/${this.state.movie.id}.jpg`)}
-                alt={`${this.state.movie.id}`}
-              />
+            <img
+              src={require(`../common/images/${movie.id}.jpg`)}
+              alt={`${movie.id}`}
+            />
             </div>
           </div>
           <Link to="/">Link to home page</Link>
